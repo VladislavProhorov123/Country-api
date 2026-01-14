404ccae464a6624d63fbdf0970bfe118db7e8ee6
Vladislav <vladislavprokhorov1005@gmail.com>
2026-01-14 20:36:37 +0200

    Country Page basic

--- src/pages/CountryPage.tsx
+++ src/pages/CountryPage.tsx
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import type { ICountry } from "../types/Country";
 
 export default function CountryPage() {
-  const {code} = useParams()
+  const { code } = useParams();
+  const [country, setCountry] = useState<ICountry | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!code) return;
+
+    fetch(
+      `https://restcountries.com/v3.1/alpha/${code}?fields=cca3,name,flags,region,subregion,population,capital`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCountry(data);
+        setLoading(false);
+      });
+  }, [code]);
+
+  if (loading) return <p className="p-10">Loading...</p>;
+  if (!country) return <p className="p-10">Country not found</p>;
+
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold">Country: {code}</h1>
+    <div className="p-10 grid md:grid-cols-2 gap-12">
+      {/* Flag */}
+      <img
+        src={country.flags.svg}
+        alt={country.name.common}
+        className="w-full rounded-xl shadow-lg"
+      />
+
+      {/* Info */}
+      <div>
+        <h1 className="text-4xl font-bold mb-6">{country.name.common}</h1>
+
+        <div className="space-y-3 text-lg">
+          <p>
+            <b>Official name:</b> {country.name.official}
+          </p>
+          <p>
+            <b>Region:</b> {country.region}
+          </p>
+          {country.subregion && (
+            <p>
+              <b>Subregion:</b> {country.subregion}
+            </p>
+          )}
+          <p>
+            <b>Population:</b> {country.population.toLocaleString()}
+          </p>
+          {country.capital && (
+            <p>
+              <b>Capital:</b> {country.capital.join(", ")}
+            </p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
