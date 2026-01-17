51213322db142baab88dac827c7ec02fe2f86fe5
Vladislav <vladislavprokhorov1005@gmail.com>
2026-01-17 15:18:02 +0200

    Update modal country, add icons and Country Page routing

--- src/components/CountryModal.tsx
+++ src/components/CountryModal.tsx
@@ -1,4 +1,6 @@
+import { BadgeInfoIcon, Heart } from "lucide-react";
 import type { ICountry } from "../types/Country";
+import { Link } from "react-router-dom";
 
 interface Props {
   country: ICountry;
@@ -11,7 +13,7 @@ export default function CountryModal({ country, onClose }: Props) {
       <div className="bg-white rounded-xl w-[400px] p-6 relative">
         <button
           onClick={onClose}
-          className="absolute top-3 right-3 text-gray-500 hover:text-black"
+          className="absolute top-1 right-3 text-gray-500 hover:text-black cursor-pointer"
         >
           ✕
         </button>
@@ -21,14 +23,26 @@ export default function CountryModal({ country, onClose }: Props) {
           alt={country.name.common}
           className="w-full h-40 object-cover rounded-md mb-4"
         />
+        <div className="flex items-start justify-between">
+          <h2 className="text-2xl font-bold mb-2">{country.name.common}</h2>
+          <div className="flex items-center gap-x-3 ">
+            <button className="cursor-pointer"><Heart fill="red" stroke="none" /></button>
+            <Link to={`/country/${country.cca3}`}
+            className="cursor-pointer"><BadgeInfoIcon /></Link>
+          </div>
+        </div>
 
-        <h2 className="text-2xl font-bold mb-2">
-          {country.name.common}
-        </h2>
-
-        <p><b>Регіон:</b> {country.region}</p>
-        <p><b>Населення:</b> {country.population.toLocaleString()}</p>
-        {country.capital && <p><b>Столиця:</b> {country.capital[0]}</p>}
+        <p>
+          <b>Регіон:</b> {country.region}
+        </p>
+        <p>
+          <b>Населення:</b> {country.population.toLocaleString()}
+        </p>
+        {country.capital && (
+          <p>
+            <b>Столиця:</b> {country.capital[0]}
+          </p>
+        )}
       </div>
     </div>
   );
--- src/types/Country.ts
+++ src/types/Country.ts
@@ -1,15 +1,37 @@
 export interface ICountry {
   cca3: string;
+
   name: {
     common: string;
-    offical: string;
+    official: string;
   };
+
   flags: {
     svg: string;
     png: string;
   };
+
   region: string;
   subregion?: string;
   population: number;
   capital?: string[];
+
+  area?: number;
+
+  languages?: {
+    [key: string]: string;
+  };
+
+  currencies?: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+
+  borders?: string[];
+  timezones?: string[];
+
+  independent?: boolean;
+  unMember?: boolean;
 }
